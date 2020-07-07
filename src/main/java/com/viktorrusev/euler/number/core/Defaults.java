9cc0106550fbeb056553b7f2490ab8340a871388
Viktor Rusev <vikrusev257@gmail.com>
2020-07-08 02:29:21 +0300

    Changed defaults

--- src/main/java/com/viktorrusev/euler/number/core/Defaults.java
+++ src/main/java/com/viktorrusev/euler/number/core/Defaults.java
@@ -2,7 +2,7 @@ package com.viktorrusev.euler.number.core;
 
 public class Defaults {
     public static final int NUMBER_OF_THREADS = 16;
-    public static final int NUMBER_OF_ELEMENTS = 1000;
+    public static final int NUMBER_OF_ELEMENTS = 100000;
     public static final int NUMBER_OF_DIGITS = 10000;
     public static final boolean QUIET_MODE_STATE = false;
     public static final String OUTPUT_FILE = "output.txt";
